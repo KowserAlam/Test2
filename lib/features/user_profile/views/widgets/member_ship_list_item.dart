7fa0c563794b299cc8b36a2527220a65113ebb7f
shofi <shofizone@gmail.com>
2020-04-18 16:43:10 +0600

    Refactored !

---
+++ lib/features/user_profile/views/widgets/member_ship_list_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:p7app/features/user_profile/models/member_ship_info.dart';
+import 'package:p7app/features/user_profile/styles/profile_common_style.dart';
+
+class MemberShipListItem extends StatelessWidget {
+  final MembershipInfo memberShip;
+  MemberShipListItem({
+    @required this.memberShip,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 8),
+      decoration: BoxDecoration(
+        color: Theme.of(context).backgroundColor,
+        borderRadius: BorderRadius.circular(5),
+        boxShadow: ProfileCommonStyle.boxShadow,),
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: ListTile(
+        contentPadding: EdgeInsets.zero,
+        leading: Container(
+            height: 55,
+            width: 55,
+            color: Theme.of(context).scaffoldBackgroundColor,
+            child: Icon(FontAwesomeIcons.certificate)),
+        title: Text("AWS Certified Solutions Architect - Associate"),
+        subtitle: Text("Issue by: IBM"),
+      ),
+    );
+  }
+
+
+}
