5103e76aa49cf5b84f82eaf3e1277188c92e1718
Sadypk <kowser@ishraak.com>
2020-04-26 17:34:00 +0600

    Done with basic reference

--- test/rating_validator_test.dart
+++ test/rating_validator_test.dart
@@ -1,13 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:p7app/main_app/util/validator.dart';
 
-main(){
-  test("Valid Rating Testing, Should return null",(){
-
-    Validator validator = Validator();
-
-    var actual = validator.validateEmail("12");
-    expect(actual, null);
-
-  });
-}
+//main(){
+//  test("Valid Rating Testing, Should return null",(){
+//
+//    Validator validator = Validator();
+//
+//    var actual = validator.expertiseFieldValidate("12");
+//    expect(actual, null);
+//
+//  });
+//}
