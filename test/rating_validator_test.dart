551e97b09c5fe716ce565f33685e499e1d5d3c80
Sadypk <kowser@ishraak.com>
2020-04-26 17:37:51 +0600

    Changing test case

--- test/rating_validator_test.dart
+++ test/rating_validator_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:p7app/main_app/util/validator.dart';
 
-//main(){
+main(){
 //  test("Valid Rating Testing, Should return null",(){
 //
 //    Validator validator = Validator();
@@ -10,4 +10,4 @@ import 'package:p7app/main_app/util/validator.dart';
 //    expect(actual, null);
 //
 //  });
-//}
+}
