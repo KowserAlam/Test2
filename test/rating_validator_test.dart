76e6a276c2292ee26c5a1e65025a4b2285d45173
Sadypk <kowser@ishraak.com>
2020-04-27 13:13:32 +0600

    Fixed all skill issues(i think).

--- test/rating_validator_test.dart
+++ test/rating_validator_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:p7app/main_app/resource/strings_utils.dart';
 import 'package:p7app/main_app/util/validator.dart';
 
 main(){
@@ -9,4 +10,31 @@ main(){
     var actual = validator.expertiseFieldValidate("10");
     expect(actual, null);
   });
+
+  test("More than two decimal points, Should return Please enter a value within 0-9 and with two decimal values max",(){
+
+    Validator validator = Validator();
+
+    var actual = validator.expertiseFieldValidate("10.999");
+    expect(actual, StringUtils.twoDecimal);
+  });
+
+
+  test("Value greater than 10 input, Should return Please enter a value within 0-10",(){
+
+    Validator validator = Validator();
+
+    var actual = validator.expertiseFieldValidate("16.66");
+    expect(actual, StringUtils.valueWithinRange);
+  });
+
+  test("Value lesser than 0 input, Should return Please enter a value within 0-9 and with two decimal values max",(){
+
+    Validator validator = Validator();
+
+    var actual = validator.expertiseFieldValidate("-9");
+    expect(actual, StringUtils.twoDecimal);
+  });
+
+
 }
