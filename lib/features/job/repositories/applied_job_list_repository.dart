1ad12f60aa2b3a0076de072dc14724b8ec68c0e0
Sadypk <kowser@ishraak.com>
2020-05-04 15:03:27 +0600

    Working on applied jobs.

---
+++ lib/features/job/repositories/applied_job_list_repository.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:bot_toast/bot_toast.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:p7app/features/job/models/job.dart';
+import 'package:p7app/features/job/models/job_list_filters.dart';
+import 'package:p7app/main_app/api_helpers/api_client.dart';
+import 'package:p7app/main_app/api_helpers/urls.dart';
+import 'package:p7app/main_app/failure/error.dart';
+import 'package:p7app/main_app/resource/strings_utils.dart';
+
+/// http://dev.ishraak.com/api/job_list/?page=1&q=job&location=&category=
+/// &location_from_homepage=&keyword_from_homepage=&skill=&salaryMin=
+/// &salaryMax=&experienceMin=&experienceMax=null&datePosted=&gender=
+/// &qualification=&sort=&page_size=10
+class AppliedJobListRepository {
+  int count;
+  bool next;
+
+  Future<Either<AppError, List<JobModel>>> fetchJobList(
+      JobListFilters filters) async {
+    var _filters =
+        "?page=${filters.page}&q=${filters.searchQuery}&location=${filters.location}&category=${filters.category}"
+        "&location_from_homepage=${filters.location_from_homepage}&keyword_from_homepage=${filters.keyword_from_homepage}"
+        "&skill=${filters.skill}&salaryMin=${filters.salaryMin}&salaryMax=${filters.salaryMin}&experienceMin=${filters.experienceMin}"
+        "&experienceMax=${filters.experienceMax}&datePosted=${filters.datePosted}&gender=${filters.gender}"
+        "&qualification=${filters.qualification}&sort=${filters.sort}&page_size=${filters.page_size}";
+
+    var url = "${Urls.appliedJobListUrl}";
+
+    try {
+      var response = await ApiClient().getRequest(url);
+      debugPrint(url);
+      print(response.statusCode);
+//      print(response.body);
+      if (response.statusCode == 200) {
+        var mapData = json.decode(response.body);
+        var jobList = fromJson(mapData);
+        return Right(jobList);
+      } else {
+        BotToast.showText(text: StringUtils.somethingIsWrong);
+        return Left(AppError.serverError);
+      }
+    } on SocketException catch (e) {
+      print(e);
+      BotToast.showText(text: StringUtils.checkInternetConnectionMessage);
+      return Left(AppError.networkError);
+    } catch (e) {
+      print(e);
+      BotToast.showText(text: StringUtils.somethingIsWrong);
+      return Left(AppError.serverError);
+    }
+  }
+
+  List<JobModel> fromJson(Map<String, dynamic> json) {
+    count = json['count'];
+    next = json['next_pages'];
+    List<JobModel> jobList = new List<JobModel>();
+    if (json['results'] != null) {
+      json['results'].forEach((v) {
+        jobList.add(new JobModel.fromJson(v));
+      });
+    }
+
+    return jobList;
+  }
+}
---
+++ lib/features/job/view/widgets/applied_job_list_screen.dart
@@ -0,0 +1,195 @@
+import 'package:after_layout/after_layout.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:p7app/features/auth/view/widgets/custom_text_field_rounded.dart';
+import 'package:p7app/features/job/view/job_details.dart';
+import 'package:p7app/features/job/view_model/job_list_view_model.dart';
+import 'package:p7app/features/job/models/job.dart';
+import 'package:p7app/features/job/view/widgets/job_list_tile_widget.dart';
+import 'package:p7app/main_app/app_theme/app_theme.dart';
+import 'package:p7app/main_app/flavour/flavor_banner.dart';
+import 'package:p7app/main_app/resource/strings_utils.dart';
+import 'package:p7app/main_app/widgets/app_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:p7app/main_app/widgets/custom_text_from_field.dart';
+import 'package:p7app/main_app/widgets/loader.dart';
+import 'package:provider/provider.dart';
+
+class AppliedJobListScreen extends StatefulWidget {
+  AppliedJobListScreen({Key key}) : super(key: key);
+
+  @override
+  _AppliedJobListScreenState createState() => _AppliedJobListScreenState();
+}
+
+class _AppliedJobListScreenState extends State<AppliedJobListScreen>
+    with AfterLayoutMixin, TickerProviderStateMixin {
+  ScrollController _scrollController = ScrollController();
+  AnimationController controller;
+  TextEditingController _searchTextEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 400),
+      reverseDuration: Duration(milliseconds: 400),
+    );
+    super.initState();
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) {
+    var jobListViewModel =
+    Provider.of<JobListViewModel>(context, listen: false);
+    jobListViewModel.getJobList();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent &&
+          jobListViewModel.hasMoreData &&
+          !jobListViewModel.isFetchingData) {
+        jobListViewModel.getMoreData();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlavorBanner(
+      child:
+      Consumer<JobListViewModel>(builder: (context, jobListViewModel, _) {
+        var jobList = jobListViewModel.jobList;
+        var isInSearchMode = jobListViewModel.isInSearchMode;
+        debugPrint("${jobList.length}");
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(StringUtils.jobListText),
+            actions: [
+              IconButton(
+                icon: Icon(isInSearchMode ? Icons.close : Icons.search),
+                onPressed: () {
+                  _searchTextEditingController?.clear();
+                  jobListViewModel.toggleIsInSearchMode();
+                },
+              )
+            ],
+          ),
+          drawer: Drawer(
+              child: AppDrawer(
+                routeName: 'job_list',
+              )),
+          body: RefreshIndicator(
+            onRefresh: () async {
+              return Provider.of<JobListViewModel>(context, listen: false)
+                  .refresh();
+            },
+            child: Column(
+              children: [
+                if (jobListViewModel.isInSearchMode)
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8,8,8,8),
+                    child: CustomTextFormField(
+                      controller: _searchTextEditingController,
+                      onChanged: jobListViewModel.addSearchQuery,
+                      hintText: StringUtils.searchText,
+                    ),
+                  ),
+                Expanded(
+                  child: ListView(
+                    physics: AlwaysScrollableScrollPhysics(),
+                    controller: _scrollController,
+                    children: [
+
+
+                      if (jobListViewModel.isFetchingData)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Loader(),
+                        ),
+                      (jobListViewModel.jobList.length == 0 && jobListViewModel.isFetchingData)
+                          ? Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(StringUtils.noJobsFound),
+                        ),
+                      )
+                          : ListView.builder(
+                          padding: EdgeInsets.symmetric(vertical: 4),
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+
+                          itemCount: jobList.length + 1,
+//              separatorBuilder: (context,index)=>Divider(),
+                          itemBuilder: (context, index) {
+                            if (index == jobList.length) {
+                              return jobListViewModel.isFetchingMoreData
+                                  ? Padding(
+                                  padding: EdgeInsets.all(15),
+                                  child: Loader())
+                                  : SizedBox();
+                            }
+
+                            JobModel job = jobList[index];
+
+                            return JobListTileWidget(
+                              job,
+                              onTap: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => JobDetails(
+                                      jobModel: job,
+                                      index: index,
+                                    )));
+                              },
+                              onFavorite: () {
+                                jobListViewModel.addToFavorite(job.jobId, index);
+                              },
+                              onApply: job.isApplied
+                                  ? null
+                                  : () {
+                                _showApplyForJobDialog(job, index);
+                              },
+                            );
+                          }),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }),
+    );
+  }
+
+  _showApplyForJobDialog(JobModel jobModel, int index) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(StringUtils.doYouWantToApplyText),
+            actions: [
+              RawMaterialButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(StringUtils.noText),
+              ),
+              RawMaterialButton(
+                onPressed: () {
+                  Provider.of<JobListViewModel>(context, listen: false)
+                      .applyForJob(jobModel.jobId, index);
+                  Navigator.pop(context);
+                },
+                child: Text(StringUtils.yesText),
+              ),
+            ],
+          );
+        });
+  }
+}
