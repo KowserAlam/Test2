32972018db929328b03ba316f74a62333f312e5d
Sadypk <kowser@ishraak.com>
2020-05-11 14:39:12 +0600

    Working on company list.

--- lib/main_app/resource/strings_utils.dart
+++ lib/main_app/resource/strings_utils.dart
@@ -171,6 +171,9 @@ class StringUtils {
 
   static String saveText = "Save";
 
+  //Company List Screen
+  static var companyListText = "Company List";
+
   //Reference Edit Screen
   static var referenceDescriptionText = "Description";
   static var referenceAppbarText = "Reference";
