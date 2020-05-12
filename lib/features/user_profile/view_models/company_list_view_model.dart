ad931d33afb9345b314a9aa3ecd13d2d40f11b65
Sadypk <kowser@ishraak.com>
2020-05-12 11:16:27 +0600

    Just Committing.

---
+++ lib/features/user_profile/view_models/company_list_view_model.dart
@@ -0,0 +1,109 @@
+import 'package:bot_toast/bot_toast.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:p7app/features/job/models/job.dart';
+import 'package:p7app/features/job/repositories/job_details_repository.dart';
+import 'package:p7app/features/user_profile/models/company.dart';
+import 'package:p7app/features/user_profile/repositories/company_list_repository.dart';
+import 'package:p7app/main_app/api_helpers/api_client.dart';
+import 'package:p7app/main_app/auth_service/auth_service.dart';
+import 'package:p7app/main_app/failure/error.dart';
+
+class CompanyListViewModel with ChangeNotifier{
+  List<Company> companyList;
+  bool isFetchingData;
+  CompanyListRepository _companyListRepository = CompanyListRepository();
+
+
+  Future<bool> getJobDetails() async {
+    isFetchingData = true;
+
+
+    Either<AppError, List<Company>> result =
+    await _companyListRepository.getList();
+    return result.fold((l) {
+      isFetchingData = false;
+      print(l);
+      return false;
+    }, (List<Company> dataModel) {
+      print(dataModel.length);
+      companyList = dataModel;
+      isFetchingData = false;
+      notifyListeners();
+      return true;
+    });
+  }
+
+//  Future<bool> applyForJob(String jobId, int index,
+//      {ApiClient apiClient}) async {
+//    BotToast.showLoading();
+//    var userId =
+//    await AuthService.getInstance().then((value) => value.getUser().userId);
+//    var body = {'user_id': userId, 'job_id': jobId};
+//
+//    try {
+//      ApiClient client = apiClient ?? ApiClient();
+//      var res = await client.postRequest(Urls.applyJobOnlineUrl, body);
+//      print(res.body);
+//
+//      if (res.statusCode == 200) {
+//        BotToast.closeAllLoading();
+//        BotToast.showText(
+//            text: StringUtils.successfullyAppliedText,
+//            duration: Duration(seconds: 2));
+//        _jobList[index].isApplied = true;
+//        notifyListeners();
+//        return true;
+//      } else {
+//        BotToast.closeAllLoading();
+//        BotToast.showText(text: StringUtils.unableToSaveData);
+//        return false;
+//      }
+//    } catch (e) {
+//      BotToast.closeAllLoading();
+//      BotToast.showText(text: StringUtils.unableToSaveData);
+//      print(e);
+//
+//      return false;
+//    }
+//  }
+//
+//  Future<bool> addToFavorite(String jobId, int index,
+//      {ApiClient apiClient}) async {
+//    BotToast.showLoading();
+//    var userId =
+//    await AuthService.getInstance().then((value) => value.getUser().userId);
+//    var body = {'user_id': userId, 'job_id': jobId};
+//
+//    try {
+//      ApiClient client = apiClient ?? ApiClient();
+//      var res = await client.postRequest(Urls.favouriteJobAddUrl, body);
+//      print(res.body);
+//
+//      if (res.statusCode == 200) {
+//        BotToast.closeAllLoading();
+//
+//        _jobList[index].status = !_jobList[index].status;
+//        notifyListeners();
+//        return true;
+//      } else {
+//        BotToast.closeAllLoading();
+//        BotToast.showText(text: StringUtils.unableToSaveData);
+//        return false;
+//      }
+//    } catch (e) {
+//      BotToast.closeAllLoading();
+//      BotToast.showText(text: StringUtils.unableToSaveData);
+//      print(e);
+//
+//      return false;
+//    }
+//  }
+
+  resetState() {
+    companyList = null;
+    isFetchingData = false;
+    _companyListRepository = CompanyListRepository();
+    notifyListeners();
+  }
+}
--- lib/features/user_profile/views/screens/company_list_screen.dart
+++ lib/features/user_profile/views/screens/company_list_screen.dart
@@ -15,7 +15,6 @@ class CompanyListScreen extends StatefulWidget {
 class _CompanyListScreenState extends State<CompanyListScreen> {
   TextEditingController _companyNameController = TextEditingController();
   Debouncer _debouncer = Debouncer(milliseconds: 400);
-  var _companyAutocompleteKey = new GlobalKey<AutoCompleteTextFieldState<Company>>();
   List<Company> companySuggestion = [];
   FocusNode _companyNameFocusNode;
   Company selectedCompany;
@@ -26,7 +25,6 @@ class _CompanyListScreenState extends State<CompanyListScreen> {
     // TODO: implement initState
     _companyNameController.addListener(() {
       if (_companyNameController.text.length > 3) {
-        _companyAutocompleteKey.currentState.suggestions = [];
         _debouncer.run(() {
           CompanyListRepository()
               .getList(query: _companyNameController.text)
@@ -34,12 +32,11 @@ class _CompanyListScreenState extends State<CompanyListScreen> {
             value.fold((l) {
               //left
               print(l);
-              _companyAutocompleteKey.currentState.suggestions = [];
             }, (List<Company> r) {
 //              //right
               companySuggestion = r;
-              _companyAutocompleteKey.currentState.updateSuggestions(r);
-              _companyAutocompleteKey.currentState.updateOverlay();
+              //_companyAutocompleteKey.currentState.updateSuggestions(r);
+              //_companyAutocompleteKey.currentState.updateOverlay();
 
 //            setState(() {
 //
@@ -54,59 +51,6 @@ class _CompanyListScreenState extends State<CompanyListScreen> {
 
   @override
   Widget build(BuildContext context) {
-    var nameOfCompany = Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text("  " + StringUtils.nameOfCompany ?? "",
-            style: TextStyle(fontWeight: FontWeight.bold)),
-        SizedBox(
-          height: 5,
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Theme.of(context).backgroundColor,
-            borderRadius: BorderRadius.circular(7),
-            boxShadow: CommonStyleTextField.boxShadow,
-          ),
-          child: AutoCompleteTextField<Company>(
-            focusNode: _companyNameFocusNode,
-            decoration: InputDecoration(
-              hintText: StringUtils.currentCompanyHint,
-              border: InputBorder.none,
-              contentPadding:
-              const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              focusedBorder: CommonStyleTextField.focusedBorder(context),
-            ),
-            controller: _companyNameController,
-            itemFilter: (Company suggestion, String query) => true,
-            suggestions: companySuggestion,
-            itemSorter: (Company a, Company b) => a.name.compareTo(b.name),
-            key: _companyAutocompleteKey,
-            itemBuilder: (BuildContext context, Company suggestion) {
-              return ListTile(
-                title: Text(suggestion.name ?? ""),
-              );
-            },
-            clearOnSubmit: false,
-            itemSubmitted: (Company data) {
-              selectedCompany = data;
-//                    _companyNameController.text = data.name;
-              _companyAutocompleteKey.currentState.updateSuggestions([]);
-              setState(() {});
-            },
-          ),
-        ),
-        if (_companyNameErrorText != null)
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              _companyNameErrorText,
-              style: TextStyle(color: Colors.red),
-            ),
-          ),
-      ],
-    );
-
     return Scaffold(
       appBar: AppBar(
         title: Text(StringUtils.companyListText),
@@ -117,7 +61,9 @@ class _CompanyListScreenState extends State<CompanyListScreen> {
           height: MediaQuery.of(context).size.height,
           child: Column(
             children: [
-              nameOfCompany,
+              TextField(
+                controller: _companyNameController,
+              ),
               Container(
                 height: MediaQuery.of(context).size.height/2,
                 child: ListView.builder(
