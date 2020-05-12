a6c9753815befb6095957ba7816e9ddea5751ad0
Sadypk <kowser@ishraak.com>
2020-05-12 16:39:20 +0600

    Done with company search. Working on company details.

--- lib/features/user_profile/views/screens/company_details.dart
+++ lib/features/user_profile/views/screens/company_details.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:p7app/features/user_profile/models/company.dart';
+import 'package:p7app/main_app/resource/const.dart';
+import 'package:p7app/main_app/resource/strings_utils.dart';
 
 class CompanyDetails extends StatefulWidget {
   final Company company;
@@ -9,8 +12,129 @@ class CompanyDetails extends StatefulWidget {
 }
 
 class _CompanyDetailsState extends State<CompanyDetails> {
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    Color sectionColor = Theme.of(context).backgroundColor;
+    Color summerySectionBorderColor = Colors.grey[300];
+    Color summerySectionColor =
+    !isDarkMode ? Colors.grey[200] : Colors.grey[600];
+    Color backgroundColor = !isDarkMode ? Colors.grey[200] : Colors.grey[700];
+
+    //Styles
+    TextStyle headerTextStyle =
+    new TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+    TextStyle sectionTitleFont =
+    TextStyle(fontSize: 17, fontWeight: FontWeight.bold);
+    TextStyle descriptionFontStyle = TextStyle(fontSize: 13);
+    TextStyle topSideDescriptionFontStyle = TextStyle(
+        fontSize: 14, color: !isDarkMode ? Colors.grey[600] : Colors.grey[500]);
+    TextStyle descriptionFontStyleBold =
+    TextStyle(fontSize: 12, fontWeight: FontWeight.bold);
+    double fontAwesomeIconSize = 15;
+
+    double iconSize = 14;
+    double sectionIconSize = 20;
+    Color clockIconColor = Colors.orange;
+
+    Text jobSummeryRichText(String title, String description) {
+      return Text.rich(
+        TextSpan(children: <TextSpan>[
+          TextSpan(text: title, style: descriptionFontStyleBold),
+          TextSpan(text: description, style: descriptionFontStyle),
+        ]),
+        style: descriptionFontStyle,
+      );
+    }
+
+
+    var dividerUpperSide = Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: 80,
+            height: 80,
+            decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Colors.grey[300])),
+            margin: EdgeInsets.only(right: 10),
+            child: Image.asset(
+              kImagePlaceHolderAsset,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Flexible(
+                      child: Container(
+                        child: Text(
+                          widget.company.name != null
+                              ? widget.company.name
+                              : StringUtils.unspecifiedText,
+                          style: headerTextStyle,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      widget.company.email != null
+                          ? widget.company.email
+                          : StringUtils.unspecifiedText,
+                      style: topSideDescriptionFontStyle,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          FeatherIcons.mapPin,
+                          size: iconSize,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Flexible(
+                          child: Text(
+                            widget.company.address != null
+                                ? widget.company.address
+                                : StringUtils.unspecifiedText,
+                            style: topSideDescriptionFontStyle,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(StringUtils.companyListText),
+      ),
+      body: ,
+    );
   }
 }
--- lib/features/user_profile/views/screens/company_list_screen.dart
+++ lib/features/user_profile/views/screens/company_list_screen.dart
@@ -1,9 +1,11 @@
 import 'package:autocomplete_textfield/autocomplete_textfield.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:p7app/features/user_profile/models/company.dart';
 import 'package:p7app/features/user_profile/repositories/company_list_repository.dart';
 import 'package:p7app/features/user_profile/styles/common_style_text_field.dart';
 import 'package:p7app/features/user_profile/view_models/company_list_view_model.dart';
+import 'package:p7app/features/user_profile/views/screens/company_details.dart';
 import 'package:p7app/main_app/resource/strings_utils.dart';
 import 'package:p7app/main_app/util/debouncer.dart';
 import 'package:provider/provider.dart';
@@ -26,15 +28,6 @@ class _CompanyListScreenState extends State<CompanyListScreen> {
     super.initState();
   }
 
-  void updateSuggestion(){
-    var companyViewModel =     Provider.of<CompanyListViewModel>(context);
-    _companyNameController.addListener(() {
-      if(_companyNameController.text.length > 2){
-        companyViewModel.query = _companyNameController.text;
-
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,6 +50,8 @@ class _CompanyListScreenState extends State<CompanyListScreen> {
                     companyViewModel.query = v;
                     companyViewModel.getJobDetails();
                     print(companyViewModel.companyList.length);
+                  }else{
+                    companyViewModel.resetState();
                   }
                 },
               ),
@@ -66,7 +61,10 @@ class _CompanyListScreenState extends State<CompanyListScreen> {
                     itemBuilder: (BuildContext context, int index){
                       return GestureDetector(
                           onTap: (){
-                            print(companySuggestion[index].name);
+                            Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) => CompanyDetails(company: companySuggestion[index],)));
                           },
                           child: ListTile(title: Text(companySuggestion[index].name),));
                     }),
