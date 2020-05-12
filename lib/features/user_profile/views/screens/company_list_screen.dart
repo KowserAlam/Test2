c9a2cd9aa5902a56dd6a04fdfa506aedde6c848e
Sadypk <kowser@ishraak.com>
2020-05-12 18:16:37 +0600

    Company list searches with button & onSubmit.

--- lib/features/user_profile/views/screens/company_list_screen.dart
+++ lib/features/user_profile/views/screens/company_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:autocomplete_textfield/autocomplete_textfield.dart';
+import 'package:bot_toast/bot_toast.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:p7app/features/user_profile/models/company.dart';
@@ -6,8 +8,10 @@ import 'package:p7app/features/user_profile/repositories/company_list_repository
 import 'package:p7app/features/user_profile/styles/common_style_text_field.dart';
 import 'package:p7app/features/user_profile/view_models/company_list_view_model.dart';
 import 'package:p7app/features/user_profile/views/screens/company_details.dart';
+import 'package:p7app/main_app/resource/const.dart';
 import 'package:p7app/main_app/resource/strings_utils.dart';
 import 'package:p7app/main_app/util/debouncer.dart';
+import 'package:p7app/main_app/widgets/custom_text_field.dart';
 import 'package:provider/provider.dart';
 
 
@@ -33,6 +37,11 @@ class _CompanyListScreenState extends State<CompanyListScreen> {
   Widget build(BuildContext context) {
     var companyViewModel = Provider.of<CompanyListViewModel>(context);
     List<Company> companySuggestion = companyViewModel.companyList==null?[]:companyViewModel.companyList;
+    void updateSuggestion(){
+      companyViewModel.query = _companyNameController.text;
+      companyViewModel.getJobDetails();
+      print(companyViewModel.companyList.length);
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text(StringUtils.companyListText),
@@ -41,22 +50,28 @@ class _CompanyListScreenState extends State<CompanyListScreen> {
         child: Container(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.all(10),
           child: Column(
             children: [
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter Company Name",
-                ),
+              CustomTextField(
                 controller: _companyNameController,
-                onChanged: (v){
-                  if(v.length >2){
-                    companyViewModel.query = v;
-                    companyViewModel.getJobDetails();
-                    print(companyViewModel.companyList.length);
+                onSubmitted: (v){
+                  if(_companyNameController.text.length>2){
+                    updateSuggestion();
                   }else{
-                    companyViewModel.resetState();
+                    BotToast.showText(text: StringUtils.searchLetterCapText);
                   }
                 },
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: (){
+                    if(_companyNameController.text.length>2){
+                      updateSuggestion();
+                    }else{
+                      BotToast.showText(text: StringUtils.searchLetterCapText);
+                    }
+                  },
+                ),
               ),
               Expanded(
                 child: ListView.builder(
@@ -69,7 +84,22 @@ class _CompanyListScreenState extends State<CompanyListScreen> {
                                 CupertinoPageRoute(
                                     builder: (context) => CompanyDetails(company: companySuggestion[index],)));
                           },
-                          child: ListTile(title: Text(companySuggestion[index].name),));
+                          child: Container(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                              margin: EdgeInsets.symmetric(vertical: 5),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(2),
+                                border: Border.all(width: 1,color: Colors.grey[400]),
+                              ),
+                              child: ListTile(
+                                leading: CachedNetworkImage(
+                                  placeholder: (context, _) => Image.asset(
+                                    kImagePlaceHolderAsset,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  imageUrl: companySuggestion[index]?.profilePicture ?? "",
+                                ),
+                                title: Text(companySuggestion[index].name),)));
                     }),
               )
             ],
@@ -78,4 +108,6 @@ class _CompanyListScreenState extends State<CompanyListScreen> {
       ),
     );
   }
+
+
 }
