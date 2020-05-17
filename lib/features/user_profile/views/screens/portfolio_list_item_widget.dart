355de1ea44e750022d20663bf56b6c7ba817dfcc
shofi <shofi@ishraak.com>
2020-05-17 16:11:46 +0600

    portfolio issue fixed !

--- lib/features/user_profile/views/screens/portfolio_list_item_widget.dart
+++ lib/features/user_profile/views/screens/portfolio_list_item_widget.dart
@@ -4,8 +4,9 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:p7app/features/user_profile/models/portfolio_info.dart';
 import 'package:p7app/features/user_profile/styles/common_style_text_field.dart';
 import 'package:p7app/main_app/resource/const.dart';
+import 'package:p7app/main_app/resource/strings_utils.dart';
 
-class PortfolioListItemWidget extends StatelessWidget {
+class PortfolioListItemWidget extends StatefulWidget {
   final PortfolioInfo portfolioInfo;
   final Function onTapEdit;
   final bool isInEditMode;
@@ -17,7 +18,26 @@ class PortfolioListItemWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _PortfolioListItemWidgetState createState() => _PortfolioListItemWidgetState();
+}
+
+class _PortfolioListItemWidgetState extends State<PortfolioListItemWidget> {
+
+  bool isExpanded = false;
+  int chLength = 150;
+
+
+
+  @override
   Widget build(BuildContext context) {
+
+    bool hasMoreText = widget.portfolioInfo.description == null
+        ? false
+        : widget.portfolioInfo.description.length > chLength;
+    String descriptionText = (isExpanded || !hasMoreText)
+        ? widget.portfolioInfo?.description ?? ""
+        : widget.portfolioInfo?.description?.substring(0, chLength) ?? "";
+
     return Container(
       margin: EdgeInsets.only(bottom: 8),
       decoration: BoxDecoration(
@@ -31,7 +51,7 @@ class PortfolioListItemWidget extends StatelessWidget {
         leading: CachedNetworkImage(
           height: 55,
           width: 55,
-          imageUrl: portfolioInfo.image??"",
+          imageUrl: widget.portfolioInfo.image??"",
           placeholder: (context, _) => Image.asset(
             kImagePlaceHolderAsset,
             height: 55,
@@ -39,22 +59,50 @@ class PortfolioListItemWidget extends StatelessWidget {
           ),
           fit: BoxFit.cover,
         ),
-        title: Text(portfolioInfo.name ?? ""),
-        subtitle: Text(portfolioInfo.description??""),
-        trailing:  !isInEditMode
-            ? SizedBox()
+        title: Text(widget.portfolioInfo.name ?? "",maxLines: 1,),
+        subtitle: Column(
+//          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Expanded(child: Text(descriptionText??"")),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            if (hasMoreText)
+              InkWell(
+                onTap: () {
+                  isExpanded = !isExpanded;
+                  setState(() {});
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(3.0),
+                  child: Text(
+                    isExpanded
+                        ? StringUtils.seeLessText
+                        : StringUtils.seeMoreText,
+                    style: TextStyle(color: Theme.of(context).primaryColor),
+                  ),
+                ),
+              )
+          ],
+        ),
+        trailing:  !widget.isInEditMode
+            ? null
             : Row(
           mainAxisSize: MainAxisSize.min,
           children: [
             IconButton(
               icon: Icon(FontAwesomeIcons.edit),
-              onPressed: onTapEdit,
+              onPressed: widget.onTapEdit,
               iconSize: 18,
               color: Colors.black,
             ),
             IconButton(
               icon: Icon(FontAwesomeIcons.trash),
-              onPressed: onTapDelete,
+              onPressed: widget.onTapDelete,
               iconSize: 18,
               color: Colors.black,
             ),
