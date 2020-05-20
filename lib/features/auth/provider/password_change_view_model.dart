ed374e93840672ace2496122b847821bf6157244
shofi <shofi@ishraak.com>
2020-05-20 11:18:21 +0600

    error text getter setter added !

--- lib/features/auth/provider/password_change_view_model.dart
+++ lib/features/auth/provider/password_change_view_model.dart
@@ -3,6 +3,31 @@ import 'package:p7app/main_app/api_helpers/api_client.dart';
 import 'package:p7app/main_app/api_helpers/urls.dart';
 
 class PasswordChangeViewModel with ChangeNotifier {
+
+  String _errorTextOldPassword;
+  String _errorTextNewPassword;
+  String _errorTextConfirmPassword;
+
+
+  String get errorTextOldPassword => _errorTextOldPassword;
+
+  set errorTextOldPassword(String value) {
+    _errorTextOldPassword = value;
+  }
+
+
+  String get errorTextNewPassword => _errorTextNewPassword;
+
+  set errorTextNewPassword(String value) {
+    _errorTextNewPassword = value;
+  }
+
+  String get errorTextConfirmPassword => _errorTextConfirmPassword;
+
+  set errorTextConfirmPassword(String value) {
+    _errorTextConfirmPassword = value;
+  }
+
   changePassword(
       {@required String oldPassword, @required String newPassword}) {
 
