45a89fb101d5fd8c8cfd2b04db6a08ad6cf6f8c0
Sadypk <kowser@ishraak.com>
2020-05-20 11:26:50 +0600

    Working on password change screen.

--- lib/features/auth/view/passworrd_change_screen.dart
+++ lib/features/auth/view/passworrd_change_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:p7app/features/auth/provider/password_change_view_model.dart';
 import 'package:p7app/features/auth/view/widgets/custom_text_field_rounded.dart';
 import 'package:p7app/main_app/resource/strings_utils.dart';
 import 'package:p7app/main_app/widgets/common_button.dart';
@@ -14,7 +15,7 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
 
   @override
   Widget build(BuildContext context) {
-    var changePassViewModel = Provider.of(context);
+    var changePassViewModel = Provider.of<PasswordChangeViewModel>(context);
     var currentPassword = CustomTextFieldRounded(
       controller: _currentPasswordTextController,
       labelText: StringUtils.currentPasswordText,
