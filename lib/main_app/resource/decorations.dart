a37988bec9657cf655b315e6cf741ef9db191814
Sadypk <kowser@ishraak.com>
2020-05-21 15:28:49 +0600

    Working on chat UI.

--- lib/main_app/resource/decorations.dart
+++ lib/main_app/resource/decorations.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:p7app/main_app/app_theme/app_theme.dart';
 
 var grad = LinearGradient(
     begin: Alignment.centerLeft,
@@ -15,3 +16,57 @@ BoxDecoration greyToWhiteDecoration = new BoxDecoration(
   ),
   border: Border.all(color: Colors.grey[200], width: 1)
 );
+
+BoxDecoration nMbox = BoxDecoration(
+    borderRadius: BorderRadius.circular(15),
+    color: AppTheme.mC,
+    boxShadow: [
+      BoxShadow(
+          color: AppTheme.mCD,
+          offset: Offset(10,10),
+          blurRadius: 10
+      ),
+      BoxShadow(
+          color: AppTheme.mCL,
+          offset: Offset(-10,-10),
+          blurRadius: 10
+      )
+    ]
+);
+
+BoxDecoration nMboxGrad = BoxDecoration(
+    borderRadius: BorderRadius.circular(15),
+    color: AppTheme.mC,
+    gradient: LinearGradient(
+        colors: [AppTheme.shadowColor, AppTheme.lightShadowColor],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight
+    ),
+    boxShadow: [
+      BoxShadow(
+          color: AppTheme.mCD,
+          offset: Offset(10,10),
+          blurRadius: 10
+      ),
+      BoxShadow(
+          color: AppTheme.mCL,
+          offset: Offset(-10,-10),
+          blurRadius: 10
+      )
+    ]
+);
+
+BoxDecoration nMboxInvert = BoxDecoration(
+    borderRadius: BorderRadius.circular(15),
+    color: AppTheme.mCD,
+    boxShadow: [
+      BoxShadow(
+          color: AppTheme.mCL,
+          offset: Offset(3,3),
+          blurRadius: 3,
+          spreadRadius: -3
+      ),
+    ]
+);
+
+BoxDecoration nMboxInvertActive = nMboxInvert.copyWith(color: AppTheme.mCC);
