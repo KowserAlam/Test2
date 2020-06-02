25e7e2ad7a0bf99e6740808aebfd103f2279a3e2
Sadypk <kowser@ishraak.com>
2020-06-02 17:29:49 +0600

    Done with career advice page design.

--- lib/main_app/views/career_advice.dart
+++ lib/main_app/views/career_advice.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:p7app/main_app/api_helpers/urls.dart';
+import 'package:p7app/main_app/app_theme/app_theme.dart';
 import 'package:p7app/main_app/flavour/flavour_config.dart';
+import 'package:p7app/main_app/models/career_advice_model.dart';
 import 'package:p7app/main_app/resource/strings_utils.dart';
 import 'package:p7app/main_app/views/widgets/pge_view_widget.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -20,7 +22,49 @@ class _CareerAdviceScreenState extends State<CareerAdviceScreen> {
       appBar: AppBar(
         title: Text(StringUtils.careerAdviceText),
       ),
-      body: PgeViewWidget(url: url,),
+      body: ListView.builder(
+        itemCount: adviceList.length,
+        itemBuilder: (BuildContext context, int index){
+        return Container(
+          padding: EdgeInsets.all(8),
+          margin: EdgeInsets.symmetric(vertical: 5,horizontal: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            gradient: AppTheme.lightLinearGradient,
+            border: Border.all(width: 1, color: Colors.grey[300]),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(adviceList[index].adviceTitle, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+              SizedBox(height: 2,),
+              Text(adviceList[index].author, style: TextStyle(fontSize: 12,color: Colors.lightBlueAccent),),
+              SizedBox(height: 10,),
+              Text(adviceList[index].description, style: TextStyle(fontSize: 14,),maxLines: 2,overflow: TextOverflow.ellipsis,),
+              SizedBox(height: 5,),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(adviceList[index].date, style: TextStyle(color: Colors.grey),),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(color: Colors.grey[400], width: 1),
+                      borderRadius: BorderRadius.circular(3)
+                    ),
+                    child: Center(
+                      child: Text('Read More', style: TextStyle(color: Colors.blueAccent),),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        );
+      }),
     );
   }
 }
