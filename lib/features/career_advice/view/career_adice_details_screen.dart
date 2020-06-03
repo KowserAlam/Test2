335d845623dd9d2258f00c85cdd24993f3341d52
shofi <shofi@ishraak.com>
2020-06-03 16:39:56 +0600

    carer advice list done !

---
+++ lib/features/career_advice/view/career_adice_details_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CareerAdviceDetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(children: [
+
+      ],),
+    );
+  }
+}
