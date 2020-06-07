f76de042ccbbfd190ae7b958c800091deba464a3
shofi <shofi@ishraak.com>
2020-06-07 11:34:24 +0600

    company issue fixed !

--- lib/features/company/view_model/company_list_view_model.dart
+++ lib/features/company/view_model/company_list_view_model.dart
@@ -4,8 +4,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:p7app/features/company/models/company.dart';
 import 'package:p7app/features/company/repositories/company_list_repository.dart';
 import 'package:p7app/main_app/failure/app_error.dart';
+import 'package:p7app/main_app/util/method_extension.dart';
 
-class CompanyListViewModel with ChangeNotifier{
+class CompanyListViewModel with ChangeNotifier {
   List<Company> companyList;
   bool isFetchingData = false;
   CompanyListRepository _companyListRepository = CompanyListRepository();
@@ -13,17 +14,19 @@ class CompanyListViewModel with ChangeNotifier{
   int noOfSearchResults = 0;
   bool searchStart = false;
 
-  set query(String value){
+  set query(String value) {
     _query = value;
+    notifyListeners();
   }
 
+  bool get shouldShowCompanyCount => _query.isNotEmptyOrNotNull && searchStart;
 
   Future<bool> getJobDetails() async {
     isFetchingData = true;
     searchStart = true;
-
+    notifyListeners();
     Either<AppError, List<Company>> result =
-    await _companyListRepository.getList(query: _query);
+        await _companyListRepository.getList(query: _query);
     return result.fold((l) {
       isFetchingData = false;
       print(l);
