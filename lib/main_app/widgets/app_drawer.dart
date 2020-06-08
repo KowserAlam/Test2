60405dd9cf3b278bd06e4931878c82500519adca
shofi <shofi@ishraak.com>
2020-06-08 13:06:01 +0600

    working on job on map!

--- lib/main_app/widgets/app_drawer.dart
+++ lib/main_app/widgets/app_drawer.dart
@@ -134,17 +134,17 @@ class _AppDrawerState extends State<AppDrawer> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  ///Home / Jobs on map index = 0
-                  DrawerListWidget(
-                    label: StringUtils.jobsOnMapText,
-                    icon: FontAwesomeIcons.map,
-                    isSelected: false,
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.of(context).push(CupertinoPageRoute(
-                          builder: (context) => JobsOnMap()));
-                    },
-                  ),
+//                  ///Home / Jobs on map index = 0
+//                  DrawerListWidget(
+//                    label: StringUtils.jobsOnMapText,
+//                    icon: FontAwesomeIcons.map,
+//                    isSelected: false,
+//                    onTap: () {
+//                      Navigator.pop(context);
+//                      Navigator.of(context).push(CupertinoPageRoute(
+//                          builder: (context) => JobsOnMap()));
+//                    },
+//                  ),
 
 //                Divider(height: 1),
 //// favorite jobs
