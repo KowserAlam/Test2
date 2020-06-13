b5e3fcfd2b170ed9d268ef7aba29d1c0edd74002
Sadypk <kowser@ishraak.com>
2020-06-13 09:08:34 +0600

    Working on jobs on map.

---
+++ lib/features/jobs_on_map/LocationPicker.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocationPicker extends StatefulWidget {
+  @override
+  _LocationPickerState createState() => _LocationPickerState();
+}
+
+class _LocationPickerState extends State<LocationPicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
