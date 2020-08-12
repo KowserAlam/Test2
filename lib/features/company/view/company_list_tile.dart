e7c9e3b4e3b15b48769ef595769bad3fc3618c11
shofi <shofi@ishraak.com>
2020-08-12 16:49:23 +0600

    company list tile ripple effect not showing issue fixed !

--- lib/features/company/view/company_list_tile.dart
+++ lib/features/company/view/company_list_tile.dart
@@ -3,13 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:p7app/features/company/models/company.dart';
 import 'package:p7app/main_app/app_theme/app_theme.dart';
+import 'package:p7app/main_app/app_theme/common_style.dart';
 import 'package:p7app/main_app/resource/const.dart';
 import 'package:p7app/main_app/resource/strings_resource.dart';
 import 'package:p7app/main_app/util/date_format_uitl.dart';
 
 class CompanyListTile extends StatefulWidget {
   final Company company;
-  CompanyListTile({@required this.company});
+  final Function onTap;
+
+  CompanyListTile({@required this.company,this.onTap});
 
   @override
   _CompanyListTileState createState() => _CompanyListTileState();
@@ -27,57 +30,51 @@ class _CompanyListTileState extends State<CompanyListTile> {
 
     return Container(
       decoration: BoxDecoration(
-          color: scaffoldBackgroundColor,
+          color: backgroundColor, boxShadow: CommonStyle.boxShadow
 //        borderRadius: BorderRadius.circular(5),
-          boxShadow: [
-            BoxShadow(
-                color:
-                Colors.black.withOpacity(0.1),
-                blurRadius: 10),
-            BoxShadow(
-                color:
-                Colors.black.withOpacity(0.2),
-                blurRadius: 10),
-          ]),
+//          boxShadow: [
+//            BoxShadow(
+//                color:
+//                Colors.black.withOpacity(0.1),
+//                blurRadius: 10),
+//            BoxShadow(
+//                color:
+//                Colors.black.withOpacity(0.2),
+//                blurRadius: 10),
+//          ],
+          ),
       margin: EdgeInsets.fromLTRB(0, 4, 0, 4),
-      child: Column(
-        crossAxisAlignment:
-        CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            color: backgroundColor,
-            padding: EdgeInsets.all(8),
-            child: Row(
-              crossAxisAlignment:
-              CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: 60,
-                  width: 60,
-                  decoration: BoxDecoration(
-                    color:
-                    scaffoldBackgroundColor,
-                  ),
-                  child: CachedNetworkImage(
-                    imageUrl:
-                    widget.company
-                        .profilePicture ??
-                        "",
-                    placeholder: (context, _) =>
-                        Image.asset(
-                            kCompanyImagePlaceholder),
-                  ),
-                ),
-                SizedBox(width: 8),
-                Expanded(
-                    child: Column(
-                      crossAxisAlignment:
-                      CrossAxisAlignment.start,
+      child: Material(
+        type: MaterialType.transparency,
+        child: InkWell(
+          onTap: widget.onTap,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: scaffoldBackgroundColor,
+                      ),
+                      child: CachedNetworkImage(
+                        imageUrl: widget.company.profilePicture ?? "",
+                        placeholder: (context, _) =>
+                            Image.asset(kCompanyImagePlaceholder),
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Expanded(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Text(
-                          widget.company
-                              .name ??
-                              "",
+                          widget.company.name ?? "",
                           style: titleStyle,
                         ),
 //                                                SizedBox(height: 3),
@@ -86,16 +83,13 @@ class _CompanyListTileState extends State<CompanyListTile> {
 //                                                  style: TextStyle(color: subtitleColor),
 //                                                ),
                         SizedBox(height: 10),
-                        if (widget.company
-                            .address !=
-                            null)
+                        if (widget.company.address != null)
                           Container(
                             child: Row(
                               children: <Widget>[
                                 Icon(
                                   FeatherIcons.mapPin,
-                                  color:
-                                  subtitleColor,
+                                  color: subtitleColor,
                                   size: iconSize,
                                 ),
                                 SizedBox(
@@ -103,16 +97,10 @@ class _CompanyListTileState extends State<CompanyListTile> {
                                 ),
                                 Expanded(
                                   child: Text(
-                                    widget.company
-                                        .address ??
-                                        "",
+                                    widget.company.address ?? "",
                                     maxLines: 1,
-                                    overflow:
-                                    TextOverflow
-                                        .ellipsis,
-                                    style: TextStyle(
-                                        color:
-                                        subtitleColor),
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(color: subtitleColor),
                                   ),
                                 )
                               ],
@@ -120,48 +108,40 @@ class _CompanyListTileState extends State<CompanyListTile> {
                           ),
                       ],
                     )),
-                SizedBox(width: 8),
-              ],
-            ),
-          ),
-          SizedBox(height: 1),
-          Container(
-            padding: EdgeInsets.all(8),
-            color: backgroundColor,
-            child: Row(
-              mainAxisAlignment:
-              MainAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Text(
-                      StringResources.companyListYearOfEstablishmentText,
-                      style: TextStyle(
-                          color: subtitleColor,
-                          fontWeight:
-                          FontWeight.w600),
-                    ),
-                    SizedBox(width: 5),
-                    Text(
-                      widget.company
-                          .yearOfEstablishment !=
-                          null
-                          ? DateFormatUtil.formatDate(
-                          widget.company
-                              .yearOfEstablishment)
-                          : StringResources
-                          .noneText,
-                      style: TextStyle(
-                          color: subtitleColor,
-                          fontWeight:
-                          FontWeight.w100),
-                    ),
+                    SizedBox(width: 8),
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+              Divider(height: 1),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          StringResources.companyListYearOfEstablishmentText,
+                          style: TextStyle(
+                              color: subtitleColor, fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          widget.company.yearOfEstablishment != null
+                              ? DateFormatUtil.formatDate(
+                                  widget.company.yearOfEstablishment)
+                              : StringResources.noneText,
+                          style: TextStyle(
+                              color: subtitleColor, fontWeight: FontWeight.w100),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
