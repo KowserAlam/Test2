4a5a513a2c20cbbd953aa4219faee4bbf2d420b2
shofi <shofi@ishraak.com>
2020-08-16 15:37:08 +0600

    logo changed !

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:p7app/features/settings/settings_view_model.dart';
 import 'package:p7app/main_app/api_helpers/urls.dart';
 import 'package:p7app/main_app/p7_app.dart';
-import 'package:flutter/material.dart';
 import 'package:p7app/main_app/views/widgets/restart_widget.dart';
-import 'package:provider/provider.dart';
-import 'package:uuid/uuid.dart';
-import 'main_app/util/locator.dart';
 
 import 'main_app/flavour/flavour_config.dart';
+import 'main_app/util/locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
