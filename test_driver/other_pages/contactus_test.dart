1dc3268e70fb1f9a58b7a9677ee9f735a51912aa
Rahat <rahat@ishraak.com>
2020-08-18 14:01:07 +0600

    Contact us test case added

--- test_driver/other_pages/contactus_test.dart
+++ test_driver/other_pages/contactus_test.dart
@@ -11,6 +11,12 @@ Future<void> contactUSTest()async{
 
   group('Contact Us Test :', () {
 
+    final dashBoardContactUsTile = find.byValueKey('dashBoardContactUsTile');
+    final dashBoardFAQTile = find.byValueKey('dashBoardFAQTile');
+    final dashBoardAboutUsTile = find.byValueKey('dashBoardAboutUsTile');
+    final dashBoardListview = find.byValueKey('dashBoardListview');
+
+
     FlutterDriver driver;
     // Connect to the Flutter driver before running any tests.
     setUpAll(() async {
@@ -25,9 +31,18 @@ Future<void> contactUSTest()async{
     });
 
     //test cases are started from here
+    test('Click on Dashboard from bottom navigation bar', () async {
+      await driver.tap(Keys.bottomNavigationBarDashboard);
+      await Future.delayed(const Duration(seconds: 10), () {});
+    });
 
-
+    test('Click on contact us tile', () async {
+      await driver.scrollUntilVisible(dashBoardListview, dashBoardContactUsTile,
+          dyScroll: -600);
+      await driver.tap(dashBoardContactUsTile);
+      await driver.tap(Keys.backButton);
+    });
   });
 
 }
-//flutter drive --flavor dev --target=test_driver/
+//flutter drive --flavor dev --target=test_driver/other_pages/contactus.dart
