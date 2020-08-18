5d7ecd7e30a9673e784da28a206555d763091653
Rahat <rahat@ishraak.com>
2020-08-18 16:57:41 +0600

    Contact us test case added and key added on behalf of them

--- test_driver/keys.dart
+++ test_driver/keys.dart
@@ -128,12 +128,12 @@ class Keys{
   static final checkFavoriteUnfavoriteFromFavoriteList = find.byValueKey('favoriteFavoriteButtonKey0'); //click on 1st tile
 
   //Contact Us
-  static final tapOnNameField = find.text('Name');
-  static final tapOnEmailField = find.text('Email');
-  static final tapOnPhoneField = find.text('Phone');
-  static final tapOnSubjectField = find.text('Subject');
-  static final tapOnMessageField = find.text('Message');
-  static final tapOnSubmitButton = find.text('Submit');
+  static final tapOnNameField = find.byValueKey('contactUsNameField');
+  static final tapOnEmailField = find.text('contactUsEmailTextField');
+  static final tapOnPhoneField = find.text('contactUsPhoneTextField');
+  static final tapOnSubjectField = find.text('contactUsSubjectTextField');
+  static final tapOnMessageField = find.text('contactUsMessageTextField');
+  static final tapOnSubmitButton = find.text('contactUsSubmitButtonKey');
 
 
 
