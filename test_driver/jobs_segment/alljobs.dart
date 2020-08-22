25a3cbf1bc4c0a79a8a2079069b3e3b087f70836
shofi <shofi@ishraak.com>
2020-08-22 18:36:37 +0600

    bash file modified !

--- test_driver/jobs_segment/alljobs.dart
+++ test_driver/jobs_segment/alljobs.dart
@@ -10,3 +10,5 @@ void main() {
   // any widget you are interested in testing.
   app.main();
 }
+//flutter drive --flavor dev --target=test_driver/job_segment/alljobs.dart
+//flutter drive --target=test_driver/job_segment/alljobs.dart -d macOS
