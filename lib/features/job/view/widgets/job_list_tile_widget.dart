b01e2b34c2c6e769ce72d417c7cd57f7fd42a199
shofi <shofi@ishraak.com>
2020-08-29 11:06:33 +0600

    image null issue fixed !

--- lib/features/job/view/widgets/job_list_tile_widget.dart
+++ lib/features/job/view/widgets/job_list_tile_widget.dart
@@ -62,7 +62,7 @@ class _JobListTileWidgetState extends State<JobListTileWidget> {
         color: scaffoldBackgroundColor,
       ),
       child: CachedNetworkImage(
-        imageUrl: widget.jobModel.profilePicture,
+        imageUrl: widget.jobModel.profilePicture??"",
         placeholder: (context, _) => Image.asset(kCompanyImagePlaceholder),
       ),
     ); //That pointless fruit logo
