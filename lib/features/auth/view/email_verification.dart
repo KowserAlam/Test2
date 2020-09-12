70ae4c6dc58eea1ebdbffd313d9cc1371b04151a
Sadypk <kowser@ishraak.com>
2020-09-12 15:32:42 +0600

    Adding email verification page

---
+++ lib/features/auth/view/email_verification.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class EmailVerification extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Verify Email'),),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
