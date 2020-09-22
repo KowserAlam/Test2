3771f7e732bc182d60f4d61d35620b2fef4316da
shofi <shofi@ishraak.com>
2020-09-22 20:37:43 +0600

    working on unilink job details!

--- lib/main_app/home.dart
+++ lib/main_app/home.dart
@@ -7,6 +7,7 @@ import 'package:p7app/features/auth/view/sign_in_screen.dart';
 import 'package:p7app/features/company/view/company_list_screen.dart';
 import 'package:p7app/features/dashboard/view/dash_board.dart';
 import 'package:p7app/features/job/view/all_job_list_screen.dart';
+import 'package:p7app/features/job/view/job_details_screen.dart';
 import 'package:p7app/features/job/view/jobs_screen.dart';
 import 'package:p7app/features/job/view_model/all_job_list_view_model.dart';
 import 'package:p7app/features/job/view_model/job_screen_view_model.dart';
@@ -28,14 +29,13 @@ import 'package:uni_links/uni_links.dart';
 import 'package:flutter/services.dart' show PlatformException;
 
 class Home extends StatefulWidget {
-
   Home();
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   NotificationViewModel _notiController = Get.put(NotificationViewModel());
   var _paeViewController = PageController();
   int currentIndex = 0;
@@ -50,7 +50,7 @@ class _HomeState extends State<Home> {
   _init() {
     locator<PushNotificationService>().fcmSubscribeNews();
     Future.delayed(Duration.zero).then((value) {
-      var authVM = Provider.of<AuthViewModel>(context,listen: false);
+      var authVM = Provider.of<AuthViewModel>(context, listen: false);
       if (authVM.isLoggerIn) {
         locator<LiveUpdateService>().initSocket(context);
         locator<PushNotificationService>().initPush();
@@ -62,11 +62,9 @@ class _HomeState extends State<Home> {
     initUniLinks();
   }
 
-
   _initUserdata() {
     Provider.of<UserProfileViewModel>(context, listen: false).getUserData();
   }
-
 
   Future<Null> initUniLinks() async {
     // Platform messages may fail, so we use a try/catch PlatformException.
@@ -75,13 +73,23 @@ class _HomeState extends State<Home> {
       // Parse the link and warn the user, if it is not correct,
       // but keep in mind it could be `null`.
 
-      if(initialLink != null){
+      if (initialLink != null) {
+        if (initialLink.contains('job_list')) {
+          Get.to(AllJobListScreen());
+        }
+        if (initialLink.contains('job_details/')) {
 
-        BotToast.showText(text: "$initialLink");
-        Get.to(AllJobListScreen());
+          /// ./adb shell 'am start -W -a android.intent.action.VIEW -c android.intent.category.BROWSABLE -d "unilinks://jobxprss.com/job_details/slug"'
+          var index = initialLink.indexOf('job_details/');
+          var slug = initialLink.substring(index+12);
+          logger.i({"slug": slug});
+          Get.to(JobDetailsScreen(slug: slug));
+        }
+        // BotToast.showText(text: "$initialLink");
+
       }
-      logger.i({"initialLink":initialLink});
-    } on PlatformException catch(e){
+      logger.i({"initialLink": initialLink});
+    } on PlatformException catch (e) {
       logger.e(e);
       BotToast.showText(text: "$e");
       // Handle exception by warning the user their action did not succeed
@@ -100,8 +108,6 @@ class _HomeState extends State<Home> {
 
         onTap: (int index) async {
           if (currentIndex != index) {
-
-
             // // animation
             // var offset = 0;
             // if (index > currentIndex) {
@@ -113,7 +119,6 @@ class _HomeState extends State<Home> {
             //     _paeViewController.offset + offset,
             //     duration: const Duration(milliseconds: 50),
             //     curve: Curves.easeInOut);
-
 
             _paeViewController.jumpToPage(index);
           }
@@ -148,7 +153,9 @@ class _HomeState extends State<Home> {
                   FontAwesomeIcons.home,
                 ),
               ),
-              title: Text(isLoggedIn?StringResources.dashBoardText:StringResources.homText)),
+              title: Text(isLoggedIn
+                  ? StringResources.dashBoardText
+                  : StringResources.homText)),
           //jobs
           BottomNavigationBarItem(
               icon: Padding(
@@ -215,13 +222,14 @@ class _HomeState extends State<Home> {
             controller: _paeViewController,
             children: <Widget>[
               DashBoard(
-                onTapSearch: (){
+                onTapSearch: () {
                   _paeViewController.animateToPage(1,
                       duration: const Duration(milliseconds: 400),
                       curve: Curves.easeInOut);
                   Provider.of<JobScreenViewModel>(context, listen: false)
                       .onChange(0);
-                  Provider.of<AllJobListViewModel>(context, listen: false).enableSearchMode();
+                  Provider.of<AllJobListViewModel>(context, listen: false)
+                      .enableSearchMode();
                 },
                 onTapApplied: () {
                   _paeViewController.animateToPage(1,
@@ -253,9 +261,9 @@ class _HomeState extends State<Home> {
               JobsScreen(),
 //              AppliedJobListScreen(),
 //              FavouriteJobListScreen(),
-             CompanyListScreen(),
-            MessageScreen(),
-            ProfileScreen(),
+              CompanyListScreen(),
+              MessageScreen(),
+              ProfileScreen(),
             ],
           ),
         ),
